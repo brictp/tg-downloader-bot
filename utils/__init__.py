436d831888e0181be7217498be0069d2b5a0db96
brictp <edbri@Brictp.>
2025-05-03 20:42:56 -0400

    added function to delete file after 3 minutes

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,5 +1,5 @@
 from .downloader import download_video
-from .delete_video import delete_video
+from .delete_video import delete_file
 from .logger import register_error
 
-__all__ = ["download_video", "delete_video", "register_error"]
+__all__ = ["download_video", "delete_file", "register_error"]
--- utils/delete_video.py
+++ utils/delete_video.py
@@ -2,12 +2,12 @@ import os
 import asyncio
 
 
-async def delete_video(path):
+async def delete_file(path):
     """Delete video after 3 minutes"""
     await asyncio.sleep(180)  # Esperar 3 minutos
 
     if os.path.exists(path):
         os.remove(path)
-        print(f"video deleted in {path}")
+        print(f"file deleted in {path}")
     else:
         print(f"⚠️ No se encontró el archivo: {path}")
